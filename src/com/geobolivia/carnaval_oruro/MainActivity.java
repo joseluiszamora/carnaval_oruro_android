89d154f2ecfa30af5aa24931a62711d9b11b8248
Jose Luis Zamora <jzamora@geo.gob.bo>
2014-02-03 11:41:25 -0400

    la paz, oruro

--- src/com/geobolivia/carnaval_oruro/MainActivity.java
+++ src/com/geobolivia/carnaval_oruro/MainActivity.java
@@ -1,162 +1,242 @@
 package com.geobolivia.carnaval_oruro;
 
-import java.io.File;
 import java.util.ArrayList;
 
-import org.osmdroid.api.IMapController;
-import org.osmdroid.bonuspack.kml.KmlDocument;
-import org.osmdroid.bonuspack.location.POI;
-import org.osmdroid.bonuspack.location.PicasaPOIProvider;
-import org.osmdroid.bonuspack.overlays.DefaultInfoWindow;
-import org.osmdroid.bonuspack.overlays.ExtendedOverlayItem;
-import org.osmdroid.bonuspack.overlays.FolderOverlay;
-import org.osmdroid.bonuspack.overlays.ItemizedOverlayWithBubble;
-import org.osmdroid.bonuspack.routing.OSRMRoadManager;
-import org.osmdroid.bonuspack.routing.Road;
-import org.osmdroid.bonuspack.routing.RoadManager;
-import org.osmdroid.bonuspack.routing.RoadNode;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.BoundingBoxE6;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.OverlayItem;
-import org.osmdroid.views.overlay.PathOverlay;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.geobolivia.slider_menu.adapter.NavDrawerListAdapter;
+import com.geobolivia.slider_menu.model.NavDrawerItem;
 
 public class MainActivity extends Activity {
-	MapView map;
-	protected FolderOverlay kmlOverlay; //root container of overlays from KML reading
-	public static KmlDocument kmlDocument;
+	private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+ 
+    // nav drawer title
+    private CharSequence mDrawerTitle;
+ 
+    // used to store app title
+    private CharSequence mTitle;
+ 
+    // slide menu items
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
+ 
+    private ArrayList<NavDrawerItem> navDrawerItems;
+    private NavDrawerListAdapter adapter;
+    
 	
+	@SuppressLint("NewApi")
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		
 		//Introduction
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		mTitle = mDrawerTitle = getTitle();
 
-		map = (MapView) findViewById(R.id.openmapview);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setClickable(true);
-        map.setBuiltInZoomControls(true);
-        map.setMultiTouchControls(true);
-        map.setUseDataConnection(true);
-        
-        
-        kmlDocument = new KmlDocument(); //made static to pass between activities
-                
-        // start point Oruro
-        GeoPoint startPoint = new GeoPoint(-17.961292 , -67.106058);
-        
-        IMapController mapController = map.getController();
-        mapController.setZoom(15);
-        mapController.setCenter(startPoint);
-        
-        RoadManager roadManager = new OSRMRoadManager();
-        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
-        waypoints.add(startPoint);
-        // Finish Points Oruro
-        waypoints.add(new GeoPoint(-17.971635, -67.108864));
-        waypoints.add(new GeoPoint(-17.970154, -67.114486));
-        waypoints.add(new GeoPoint(-17.969127, -67.117911));
-        waypoints.add(new GeoPoint(-17.968290, -67.117622));
-        waypoints.add(new GeoPoint(-17.968015, -67.118502));
-        waypoints.add(new GeoPoint(-17.967632, -67.119584));
-                
-        Road road = roadManager.getRoad(waypoints);
-        Log.d("CordovaLog", ">>**>>>>>> " + road.mNodes.size());
-        PathOverlay roadOverlay = RoadManager.buildRoadOverlay(road, map.getContext());
-        map.getOverlays().add(roadOverlay);
-        map.invalidate();
+		// load slide menu items
+		navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+
+		// nav drawer icons from resources
+		navMenuIcons = getResources()
+				.obtainTypedArray(R.array.nav_drawer_icons);
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
+
+		navDrawerItems = new ArrayList<NavDrawerItem>();
+
+		// adding nav drawer items to array
+		// Home
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[0], navMenuIcons.getResourceId(0, -1)));
+		// Find People
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[1], navMenuIcons.getResourceId(1, -1)));
+		// Photos
+		navDrawerItems.add(new NavDrawerItem(navMenuTitles[2], navMenuIcons.getResourceId(2, -1)));
+		// Communities, Will add a counter here
+		//navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons.getResourceId(3, -1), true, "22"));
+		// Pages
+		//navDrawerItems.add(new NavDrawerItem(navMenuTitles[4], navMenuIcons.getResourceId(4, -1)));
+		// What's hot, We  will add a counter here
+		//navDrawerItems.add(new NavDrawerItem(navMenuTitles[5], navMenuIcons.getResourceId(5, -1), true, "50+"));
 		
-        
-        //* ADD CUSTOMIZE POINTS AND BUBBLES
-        final ArrayList<ExtendedOverlayItem> roadItems = new ArrayList<ExtendedOverlayItem>();
-		ItemizedOverlayWithBubble<ExtendedOverlayItem> roadNodes = new ItemizedOverlayWithBubble<ExtendedOverlayItem>(this, roadItems, map);
-		Drawable marker = getResources().getDrawable(R.drawable.marker_poi_picasa_24);
-		
-        for (GeoPoint geoPoint : waypoints) {
-    		ExtendedOverlayItem nodeMarker = new ExtendedOverlayItem("Insert title Here", "Insert Amazing Description here", geoPoint, this);
-            nodeMarker.setMarkerHotspot(OverlayItem.HotspotPlace.CENTER);
-            nodeMarker.setMarker(marker);
-            roadNodes.addItem(nodeMarker);
-		}
-        
-        map.getOverlays().add(roadNodes);
-        map.invalidate();
-        
-   
-		//10. Loading KML content
-        
-		//String url = "http://mapsengine.google.com/map/kml?mid=z6IJfj90QEd4.kcfEKhi8r5LQ";
-		KmlDocument kmlDocument = new KmlDocument();
-		//boolean ok = kmlDocument.parseUrl(url);
-		File file = kmlDocument.getDefaultPathForAndroid("vturismo-pasarela.kml");
-		boolean ok = kmlDocument.parseFile(file);
-		Drawable defaultMarker = getResources().getDrawable(R.drawable.marker_kml_point);
-		if (ok){
-			FolderOverlay kmlOverlay = (FolderOverlay)kmlDocument.kmlRoot.buildOverlays(this, map, defaultMarker, kmlDocument, false);
-			map.getOverlays().add(kmlOverlay);
-			map.invalidate();
-			if (kmlDocument.kmlRoot.mBB != null){
-				//map.zoomToBoundingBox(kmlRoot.mBB); => not working in onCreate - this is a well-known osmdroid bug. 
-				//Workaround:
-				map.getController().setCenter(new GeoPoint(
-						kmlDocument.kmlRoot.mBB.getLatSouthE6()+kmlDocument.kmlRoot.mBB.getLatitudeSpanE6()/2, 
-						kmlDocument.kmlRoot.mBB.getLonWestE6()+kmlDocument.kmlRoot.mBB.getLongitudeSpanE6()/2));
+
+		// Recycle the typed array
+		navMenuIcons.recycle();
+
+		mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
+
+		// setting the nav drawer list adapter
+		adapter = new NavDrawerListAdapter(getApplicationContext(),
+				navDrawerItems);
+		mDrawerList.setAdapter(adapter);
+
+		// enabling action bar app icon and behaving it as toggle button
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, //nav menu toggle icon
+				R.string.app_name, // nav drawer open - description for accessibility
+				R.string.app_name // nav drawer close - description for accessibility
+		) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				// calling onPrepareOptionsMenu() to show action bar icons
+				invalidateOptionsMenu();
 			}
-		}
-		
-		
-		//11. Grab overlays in KML structure, save KML document locally
-		/*if (kmlDocument.kmlRoot != null){
-			kmlDocument.kmlRoot.addOverlay(roadOverlay, kmlDocument);
-			kmlDocument.kmlRoot.addOverlay(roadNodes, kmlDocument);
-			File localFile = kmlDocument.getDefaultPathForAndroid("my_routes.kml");
-			kmlDocument.saveAsKML(localFile);
-		}*/
-	}
-	
-	//7. Customizing the bubble behaviour
-	class CustomInfoWindow extends DefaultInfoWindow {
-		POI selectedPoi;
-		public CustomInfoWindow(MapView mapView) {
-			super(R.layout.bonuspack_bubble, mapView);
-			Button btn = (Button)(mView.findViewById(R.id.bubble_moreinfo));
-			btn.setOnClickListener(new View.OnClickListener() {
-			    public void onClick(View view) {
-			        if (selectedPoi.mUrl != null){
-			            Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(selectedPoi.mUrl));
-			            view.getContext().startActivity(myIntent);
-			        } else {
-			        	Toast.makeText(view.getContext(), "Button clicked", Toast.LENGTH_LONG).show();
-			        }
-			    }
-			});
-		}
-		@Override public void onOpen(Object item){
-			super.onOpen(item);
-			ExtendedOverlayItem eItem = (ExtendedOverlayItem)item;
-			selectedPoi = (POI)eItem.getRelatedObject();
-			mView.findViewById(R.id.bubble_moreinfo).setVisibility(View.VISIBLE);
-			
-			//8. put thumbnail image in bubble, fetching the thumbnail in background:
-			if (selectedPoi.mThumbnailPath != null){
-				ImageView imageView = (ImageView)mView.findViewById(R.id.bubble_image);
-				selectedPoi.fetchThumbnailOnThread(imageView);
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				// calling onPrepareOptionsMenu() to hide action bar icons
+				invalidateOptionsMenu();
 			}
-		}	
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+		if (savedInstanceState == null) {
+			// on first time display view for first nav item
+			displayView(0);
+		}
+		
 	}
+
 	
+	
+	
+	/**
+	 * Slide menu item click listener
+	 * */
+	private class SlideMenuClickListener implements
+			ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			// display view for selected nav drawer item
+			displayView(position);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// toggle nav drawer on selecting action bar app icon/title
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle action bar actions click
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	/* *
+	 * Called when invalidateOptionsMenu() is triggered
+	 */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// if nav drawer is opened, hide the action items
+		boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	/**
+	 * Diplaying fragment view for selected nav drawer list item
+	 * */
+	private void displayView(int position) {
+		// update the main content by replacing fragments
+		Fragment fragment = null;
+		switch (position) {
+		case 0:
+			fragment = new HomeFragment();
+			break;
+		case 1:
+			fragment = new FindPeopleFragment();
+			break;
+		case 2:
+			fragment = new PhotosFragment();
+			break;
+		case 3:
+			fragment = new CommunityFragment();
+			break;
+		case 4:
+			fragment = new PagesFragment();
+			break;
+		case 5:
+			fragment = new WhatsHotFragment();
+			break;
+
+		default:
+			break;
+		}
+
+		if (fragment != null) {
+			FragmentManager fragmentManager = getFragmentManager();
+			fragmentManager.beginTransaction()
+					.replace(R.id.frame_container, fragment).commit();
+
+			// update selected item and title, then close the drawer
+			mDrawerList.setItemChecked(position, true);
+			mDrawerList.setSelection(position);
+			setTitle(navMenuTitles[position]);
+			mDrawerLayout.closeDrawer(mDrawerList);
+		} else {
+			// error in creating fragment
+			Log.e("MainActivity", "Error in creating fragment");
+		}
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
+
+	/**
+	 * When using the ActionBarDrawerToggle, you must call it during
+	 * onPostCreate() and onConfigurationChanged()...
+	 */
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		// Pass any configuration change to the drawer toggls
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+
+
+
+
 }
