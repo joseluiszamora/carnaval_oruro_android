5692320216a73bfa1d5f5065e02b11768f1d52c2
Jose Luis Zamora <jzamora@geo.gob.bo>
2014-02-03 08:52:00 -0400

    menu

--- gen/com/geobolivia/carnaval_oruro/R.java
+++ gen/com/geobolivia/carnaval_oruro/R.java
@@ -11,7 +11,14 @@ public final class R {
     public static final class array {
         /**  39 
          */
-        public static final int direction_icons=0x7f050000;
+        public static final int direction_icons=0x7f060000;
+        /**  Nav Drawer List Item Icons 
+ Keep them in order as the titles are in 
+         */
+        public static final int nav_drawer_icons=0x7f060003;
+        /**  Nav Drawer Menu Items 
+         */
+        public static final int nav_drawer_items=0x7f060002;
         /**   
 		  Nominatim keywords that can be used for facility search. 
 		  Note that Nominatim facility search uses Special Phrases instead of OSM tags. 
@@ -24,9 +31,17 @@ public final class R {
 		  But, again, note that keywords for Nominatim are not the OSM features. 
 		
          */
-        public static final int poi_tags=0x7f050001;
+        public static final int poi_tags=0x7f060001;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int counter_text_bg=0x7f040004;
+        public static final int counter_text_color=0x7f040005;
+        public static final int list_background=0x7f040001;
+        public static final int list_background_pressed=0x7f040002;
+        public static final int list_divider=0x7f040003;
+        public static final int list_item_title=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -35,8 +50,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int bonuspack_bubble=0x7f020000;
@@ -46,132 +61,152 @@ public final class R {
         public static final int btn_route=0x7f020004;
         public static final int btn_tracking_off=0x7f020005;
         public static final int btn_tracking_on=0x7f020006;
-        public static final int delete=0x7f020007;
-        public static final int delete_pressed=0x7f020008;
-        public static final int expander_bgrnd=0x7f020009;
-        public static final int ic_arrived=0x7f02000a;
-        public static final int ic_continue=0x7f02000b;
-        public static final int ic_empty=0x7f02000c;
-        public static final int ic_launcher=0x7f02000d;
-        public static final int ic_menu_itinerary=0x7f02000e;
-        public static final int ic_menu_poi=0x7f02000f;
-        public static final int ic_roundabout=0x7f020010;
-        public static final int ic_sharp_left=0x7f020011;
-        public static final int ic_sharp_right=0x7f020012;
-        public static final int ic_slight_left=0x7f020013;
-        public static final int ic_slight_right=0x7f020014;
-        public static final int ic_turn_left=0x7f020015;
-        public static final int ic_turn_right=0x7f020016;
-        public static final int ic_u_turn=0x7f020017;
-        public static final int icon=0x7f020018;
-        public static final int kml_icon_linestring=0x7f020019;
-        public static final int kml_icon_polygon=0x7f02001a;
-        public static final int marker_departure=0x7f02001b;
-        public static final int marker_destination=0x7f02001c;
-        public static final int marker_kml_point=0x7f02001d;
-        public static final int marker_node=0x7f02001e;
-        public static final int marker_poi=0x7f02001f;
-        public static final int marker_poi_default=0x7f020020;
-        public static final int marker_poi_flickr=0x7f020021;
-        public static final int marker_poi_picasa_24=0x7f020022;
-        public static final int marker_poi_wikipedia_16=0x7f020023;
-        public static final int marker_poi_wikipedia_32=0x7f020024;
-        public static final int marker_via=0x7f020025;
-        public static final int moreinfo_arrow=0x7f020026;
-        public static final int moreinfo_arrow_pressed=0x7f020027;
+        public static final int counter_bg=0x7f020007;
+        public static final int delete=0x7f020008;
+        public static final int delete_pressed=0x7f020009;
+        public static final int expander_bgrnd=0x7f02000a;
+        public static final int ic_arrived=0x7f02000b;
+        public static final int ic_communities=0x7f02000c;
+        public static final int ic_continue=0x7f02000d;
+        public static final int ic_drawer=0x7f02000e;
+        public static final int ic_empty=0x7f02000f;
+        public static final int ic_home=0x7f020010;
+        public static final int ic_launcher=0x7f020011;
+        public static final int ic_menu_itinerary=0x7f020012;
+        public static final int ic_menu_poi=0x7f020013;
+        public static final int ic_pages=0x7f020014;
+        public static final int ic_people=0x7f020015;
+        public static final int ic_photos=0x7f020016;
+        public static final int ic_roundabout=0x7f020017;
+        public static final int ic_sharp_left=0x7f020018;
+        public static final int ic_sharp_right=0x7f020019;
+        public static final int ic_slight_left=0x7f02001a;
+        public static final int ic_slight_right=0x7f02001b;
+        public static final int ic_turn_left=0x7f02001c;
+        public static final int ic_turn_right=0x7f02001d;
+        public static final int ic_u_turn=0x7f02001e;
+        public static final int ic_whats_hot=0x7f02001f;
+        public static final int icon=0x7f020020;
+        public static final int kml_icon_linestring=0x7f020021;
+        public static final int kml_icon_polygon=0x7f020022;
+        public static final int list_item_bg_normal=0x7f020023;
+        public static final int list_item_bg_pressed=0x7f020024;
+        public static final int list_selector=0x7f020025;
+        public static final int marker_departure=0x7f020026;
+        public static final int marker_destination=0x7f020027;
+        public static final int marker_kml_point=0x7f020028;
+        public static final int marker_node=0x7f020029;
+        public static final int marker_poi=0x7f02002a;
+        public static final int marker_poi_default=0x7f02002b;
+        public static final int marker_poi_flickr=0x7f02002c;
+        public static final int marker_poi_picasa_24=0x7f02002d;
+        public static final int marker_poi_wikipedia_16=0x7f02002e;
+        public static final int marker_poi_wikipedia_32=0x7f02002f;
+        public static final int marker_via=0x7f020030;
+        public static final int moreinfo_arrow=0x7f020031;
+        public static final int moreinfo_arrow_pressed=0x7f020032;
     }
     public static final class id {
-        public static final int action_settings=0x7f09002a;
-        public static final int btnOK=0x7f090015;
-        public static final int bubble_delete=0x7f09000a;
-        public static final int bubble_description=0x7f090004;
-        public static final int bubble_image=0x7f090001;
-        public static final int bubble_moreinfo=0x7f090003;
-        public static final int bubble_subdescription=0x7f090005;
-        public static final int bubble_title=0x7f090002;
-        public static final int buttonSearchDep=0x7f09001a;
-        public static final int buttonSearchDest=0x7f09001c;
-        public static final int buttonSetPOITag=0x7f09001e;
-        public static final int buttonTrackingMode=0x7f090023;
-        public static final int description=0x7f090010;
-        public static final int details=0x7f090008;
-        public static final int editDeparture=0x7f090019;
-        public static final int editDestination=0x7f09001b;
-        public static final int expander=0x7f09001f;
-        public static final int fillColor=0x7f090014;
-        public static final int fillColorLayout=0x7f090013;
-        public static final int focusPicker=0x7f09000e;
-        public static final int items=0x7f090009;
-        public static final int listItemCheckbox=0x7f09000d;
-        public static final int listItemImg=0x7f09000b;
-        public static final int listItemTxt=0x7f09000c;
-        public static final int listviewKml=0x7f090016;
-        public static final int mainLayout=0x7f090017;
-        public static final int map=0x7f090022;
-        public static final int menu_behind=0x7f090027;
-        public static final int menu_copy=0x7f090025;
-        public static final int menu_cut=0x7f090024;
-        public static final int menu_departure=0x7f09002b;
-        public static final int menu_destination=0x7f09002c;
-        public static final int menu_front=0x7f090028;
-        public static final int menu_itinerary=0x7f09002e;
-        public static final int menu_kml=0x7f09003a;
-        public static final int menu_kml_clear=0x7f090040;
-        public static final int menu_kml_file=0x7f09003c;
-        public static final int menu_kml_get_overlays=0x7f09003d;
-        public static final int menu_kml_save=0x7f09003f;
-        public static final int menu_kml_tree=0x7f09003e;
-        public static final int menu_kml_url=0x7f09003b;
-        public static final int menu_none=0x7f090029;
-        public static final int menu_paste=0x7f090026;
-        public static final int menu_pois=0x7f09002f;
-        public static final int menu_route=0x7f090034;
-        public static final int menu_route_google=0x7f090039;
-        public static final int menu_route_mapquest_bicycle=0x7f090037;
-        public static final int menu_route_mapquest_fastest=0x7f090036;
-        public static final int menu_route_mapquest_pedestrian=0x7f090038;
-        public static final int menu_route_osrm=0x7f090035;
-        public static final int menu_tile=0x7f090030;
-        public static final int menu_tile_mapbox_satellite=0x7f090033;
-        public static final int menu_tile_mapnik=0x7f090031;
-        public static final int menu_tile_mapquest_osm=0x7f090032;
-        public static final int menu_viapoint=0x7f09002d;
-        public static final int name=0x7f09000f;
-        public static final int openmapview=0x7f090000;
-        public static final int outlineColor=0x7f090012;
-        public static final int outlineColorLayout=0x7f090011;
-        public static final int poiTag=0x7f09001d;
-        public static final int routeInfo=0x7f090020;
-        public static final int search_panel=0x7f090018;
-        public static final int speed=0x7f090021;
-        public static final int thumbnail=0x7f090006;
-        public static final int title=0x7f090007;
+        public static final int action_settings=0x7f0a002f;
+        public static final int btnOK=0x7f0a001a;
+        public static final int bubble_delete=0x7f0a000f;
+        public static final int bubble_description=0x7f0a0007;
+        public static final int bubble_image=0x7f0a0004;
+        public static final int bubble_moreinfo=0x7f0a0006;
+        public static final int bubble_subdescription=0x7f0a0008;
+        public static final int bubble_title=0x7f0a0005;
+        public static final int buttonSearchDep=0x7f0a001f;
+        public static final int buttonSearchDest=0x7f0a0021;
+        public static final int buttonSetPOITag=0x7f0a0023;
+        public static final int buttonTrackingMode=0x7f0a0028;
+        public static final int counter=0x7f0a000b;
+        public static final int description=0x7f0a0015;
+        public static final int details=0x7f0a000d;
+        public static final int drawer_layout=0x7f0a0000;
+        public static final int editDeparture=0x7f0a001e;
+        public static final int editDestination=0x7f0a0020;
+        public static final int expander=0x7f0a0024;
+        public static final int fillColor=0x7f0a0019;
+        public static final int fillColorLayout=0x7f0a0018;
+        public static final int focusPicker=0x7f0a0013;
+        public static final int frame_container=0x7f0a0001;
+        public static final int icon=0x7f0a0009;
+        public static final int items=0x7f0a000e;
+        public static final int listItemCheckbox=0x7f0a0012;
+        public static final int listItemImg=0x7f0a0010;
+        public static final int listItemTxt=0x7f0a0011;
+        public static final int list_slidermenu=0x7f0a0003;
+        public static final int listviewKml=0x7f0a001b;
+        public static final int mainLayout=0x7f0a001c;
+        public static final int map=0x7f0a0027;
+        public static final int menu_behind=0x7f0a002c;
+        public static final int menu_copy=0x7f0a002a;
+        public static final int menu_cut=0x7f0a0029;
+        public static final int menu_departure=0x7f0a0030;
+        public static final int menu_destination=0x7f0a0031;
+        public static final int menu_front=0x7f0a002d;
+        public static final int menu_itinerary=0x7f0a0033;
+        public static final int menu_kml=0x7f0a003f;
+        public static final int menu_kml_clear=0x7f0a0045;
+        public static final int menu_kml_file=0x7f0a0041;
+        public static final int menu_kml_get_overlays=0x7f0a0042;
+        public static final int menu_kml_save=0x7f0a0044;
+        public static final int menu_kml_tree=0x7f0a0043;
+        public static final int menu_kml_url=0x7f0a0040;
+        public static final int menu_none=0x7f0a002e;
+        public static final int menu_paste=0x7f0a002b;
+        public static final int menu_pois=0x7f0a0034;
+        public static final int menu_route=0x7f0a0039;
+        public static final int menu_route_google=0x7f0a003e;
+        public static final int menu_route_mapquest_bicycle=0x7f0a003c;
+        public static final int menu_route_mapquest_fastest=0x7f0a003b;
+        public static final int menu_route_mapquest_pedestrian=0x7f0a003d;
+        public static final int menu_route_osrm=0x7f0a003a;
+        public static final int menu_tile=0x7f0a0035;
+        public static final int menu_tile_mapbox_satellite=0x7f0a0038;
+        public static final int menu_tile_mapnik=0x7f0a0036;
+        public static final int menu_tile_mapquest_osm=0x7f0a0037;
+        public static final int menu_viapoint=0x7f0a0032;
+        public static final int name=0x7f0a0014;
+        public static final int openmapview=0x7f0a0002;
+        public static final int outlineColor=0x7f0a0017;
+        public static final int outlineColorLayout=0x7f0a0016;
+        public static final int poiTag=0x7f0a0022;
+        public static final int routeInfo=0x7f0a0025;
+        public static final int search_panel=0x7f0a001d;
+        public static final int speed=0x7f0a0026;
+        public static final int thumbnail=0x7f0a000c;
+        public static final int title=0x7f0a000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int bonuspack_bubble=0x7f030001;
         public static final int bonuspack_bubble_black=0x7f030002;
-        public static final int item_layout=0x7f030003;
-        public static final int items_list=0x7f030004;
-        public static final int itinerary_bubble=0x7f030005;
-        public static final int kml_list_item=0x7f030006;
-        public static final int kml_main=0x7f030007;
-        public static final int main=0x7f030008;
+        public static final int drawer_list_item=0x7f030003;
+        public static final int item_layout=0x7f030004;
+        public static final int items_list=0x7f030005;
+        public static final int itinerary_bubble=0x7f030006;
+        public static final int kml_list_item=0x7f030007;
+        public static final int kml_main=0x7f030008;
+        public static final int main=0x7f030009;
     }
     public static final class menu {
-        public static final int kml_item_menu=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int map_menu=0x7f080002;
-        public static final int option_menu=0x7f080003;
+        public static final int kml_item_menu=0x7f090000;
+        public static final int main=0x7f090001;
+        public static final int map_menu=0x7f090002;
+        public static final int option_menu=0x7f090003;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int departure=0x7f060004;
-        public static final int destination=0x7f060005;
-        public static final int hello_world=0x7f060002;
-        public static final int my_position=0x7f060003;
-        public static final int viapoint=0x7f060006;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int departure=0x7f070004;
+        /**  Content Description 
+         */
+        public static final int desc_list_item_icon=0x7f070007;
+        public static final int destination=0x7f070005;
+        public static final int hello_world=0x7f070002;
+        public static final int my_position=0x7f070003;
+        public static final int viapoint=0x7f070006;
     }
     public static final class style {
         /** 
@@ -195,10 +230,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
